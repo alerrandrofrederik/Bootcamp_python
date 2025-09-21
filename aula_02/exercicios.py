0434f7a45f0dcb2afe2bd86995ac01279dbffe12
Alerrandro <alerrandrofrederik@gmail.com>
2025-09-21 14:01:31 -0300

    add pasta aula 2

---
+++ aula_02/exercicios.py
@@ -0,0 +1,10 @@
+
+# %%
+# 4. fassa um programa que peça dois numeros inteiros a divisão inteira do primeiro pelo segundo 
+
+primeiro_numero = int(input("digite o primeiro número: "))
+segundo_numero = int(input("digite o segundo número: "))
+
+divisao = primeiro_numero // segundo_numero
+
+print('A divisão de ',primeiro_numero," por ",segundo_numero, " é igual a: ",  divisao)
