9b13d4c7a050f12557b20133051794d871b7ce0a
Alerrandro <alerrandrofrederik@gmail.com>
2025-10-19 14:06:25 -0300

    add type hint e doc string na função

--- Aula_07/etl.py
+++ Aula_07/etl.py
@@ -1,13 +1,16 @@
 import csv
 
-# Função para ler o arquivo CSV
-def ler_csv(nome_arquivo):
+def ler_csv(nome_arquivo: str) -> list:
+    '''Função para ler o arquivo CSV
+    '''
     with open(nome_arquivo, mode='r', encoding='utf-8') as arquivo:
         leitor = csv.DictReader(arquivo)
         return list(leitor)
 
-# Função para processar os dados em um dicionário
-def processar_dados(dados):
+
+def processar_dados(dados: list) -> dict:
+    '''Função para processar os dados em um dicionário
+    '''
     categorias = {}
     for item in dados:
         categoria = item['Categoria']
@@ -16,16 +19,20 @@ def processar_dados(dados):
         categorias[categoria].append(item)
     return categorias
 
-# Função para calcular o total de vendas por categoria
-def calcular_vendas_categoria(dados):
+
+def calcular_vendas_categoria(dados:dict)-> dict:
+    '''Função para calcular o total de vendas por categoria
+    '''
     vendas_por_categoria = {}
     for categoria, itens in dados.items():
         total_vendas = sum(int(item['Quantidade']) * int(item['Venda']) for item in itens)
         vendas_por_categoria[categoria] = total_vendas
     return vendas_por_categoria
 
-# Função principal para integrar as funções anteriores
+
 def main():
+    '''Função principal para integrar as funções anteriores
+    '''
     nome_arquivo = 'Aula_07/vendas.csv'
     dados_brutos = ler_csv(nome_arquivo)
     dados_processados = processar_dados(dados_brutos)
