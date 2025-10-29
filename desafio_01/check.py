951f91ce9fc873559a2b36c31f5526ad58507b6f
Alerrandro <alerrandrofrederik@gmail.com>
2025-10-29 05:37:14 -0300

    salvando arquivo com erro em csv

--- desafio_01/check.py
+++ desafio_01/check.py
@@ -27,29 +27,48 @@ for i in funcionario:
     try:
         #valida nome
         if len(i['nome']) == 0:
+            i['erro'] = 'Nome vazio'
             com_erro.append(i)
         elif any(char.isdigit() for char in i['nome']):
+            i['erro'] = 'Nome contém numeros'
             com_erro.append(i)
 
         #valida Area:
         elif i['area'] not in ('Vendas','TI','Financeiro','RH','Operações'):
+            i['erro'] = 'area fora do esperado'
             com_erro.append(i)
 
         #valida Salário:
         elif not isinstance(i['salario'], int):
+            i['erro'] = 'Tipo salario não válido'
             com_erro.append(i)
         elif i['salario'] < 0:
+            i['erro'] = 'Salario negativo'
             com_erro.append(i)
 
         #valida bonus_percentual:
         elif len(i['bonus_percentual']) == 0:
+            i['erro'] = 'bonus_percentual vazio'
             com_erro.append(i)
         elif not 0 <= float(i['bonus_percentual']) <= 1:
+            i['erro'] = 'bonus_percentual fora do intervalo esperado'
             com_erro.append(i)
 
         else:
             sem_erro.append(i)
     except ValueError:
+        i['erro'] = 'valor invalido check os valores'
         com_erro.append(i)
 
 com_erro
+# %%
+#  registros inválidos com o motivo do erro em csv
+
+with open('erros.csv', mode='w', encoding='utf-8', newline='') as file:
+    nomes_colunas = com_erro[0].keys()
+    escritor = csv.DictWriter(file, fieldnames=nomes_colunas)
+
+    escritor.writeheader()
+    escritor.writerows(com_erro)
+
+# %%
